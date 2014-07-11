c6d55b232d6ede252ad8be02d7825e396b92135e
Jason Varga <jason@pixelfear.com>
2014-07-11 15:56:28 -0400

    Create pi.field_options.php

---
+++ _add-ons/field_options/pi.field_options.php
@@ -0,0 +1,34 @@
+<?php
+
+class Plugin_field_options extends Plugin
+{
+
+	public function index()
+	{
+		// Get fieldset from parameter or front-matter
+		// Bail and log error if no fieldset
+		if ( ! $fieldset = $this->fetchParam('fieldset', array_get($this->context, '_fieldset'))) {
+			$this->log->error('Fieldset not found in front-matter or in parameters.');
+			return false;
+		}
+
+		// Get the field config
+		$field = $this->fetchParam('field');
+		$fieldset = Statamic_fieldset::fetch_fieldset($fieldset);
+		$field_config = array_get($fieldset, "fields:$field");
+
+		// Reformat the options array
+		if ($options = array_get($field_config, 'options')) {
+			foreach ($options as $key => $val) {
+				$field_config['options'][] = array(
+					'name'  => $key,
+					'value' => $val
+				);
+				unset($field_config['options'][$key]);
+			}
+		}
+
+		return Parse::tagLoop($this->content, $field_config['options'], true, $this->context);
+	}
+
+}
